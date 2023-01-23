297c78a1484403389c0ea7cf5b3f295578dd4e33
gvielza <80008338+gvielza@users.noreply.github.com>
2023-01-23 19:49:11 -0300

    traducida frase

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ from translate import Translator
 response = requests.get(f'https://favqs.com/api/qotd', headers={'Authorization': f'Token token={key}'})
 quote = response.json()
 phrase = quote["quote"]["body"]
+autor = quote["quote"]["author"]
 
 print(phrase)
 
 
 
-translator = Translator(to_lang='en')
-translation = translator.translate("Hola mundo")
+translator = Translator(to_lang='es')
+translation = translator.translate(phrase)
 print(translation)
+print(autor)
 
 
 
